309cdf773925523f461cb95b6384520a49a1a76f
Stefan Hagen <stefan@hagen.link>
2021-11-27 16:51:57 +0100

    Initial working main

--- prosessilouhinta/__main__.py
+++ prosessilouhinta/__main__.py
@@ -1,1 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+import sys
 
+from prosessilouhinta.cli import app
+
+if __name__ == '__main__':
+    sys.exit(app(prog_name='prosessilouhinta'))  # pragma: no cover
