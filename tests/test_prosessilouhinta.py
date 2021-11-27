e773acc565e8cd342d07ba7f8f1b259a121bac3c
Stefan Hagen <stefan@hagen.link>
2021-11-27 21:38:48 +0100

    Added average time differences tet (failing, WIP)

--- tests/test_prosessilouhinta.py
+++ tests/test_prosessilouhinta.py
@@ -77,3 +77,13 @@ def test_time_differences_single_case_two_users():
         ]
     }
     assert pm.time_differences(eventlog) == {'t1': {'t2': [dti.timedelta(seconds=1)]}}
+
+
+def test_average_time_differences_single_case_two_users():
+    eventlog = {
+        'c1': [
+            ('t1', 'u1', dti.datetime.strptime('2021-11-27 12:34:56', '%Y-%m-%d %H:%M:%S')),
+            ('t2', 'u1', dti.datetime.strptime('2021-11-27 12:34:57', '%Y-%m-%d %H:%M:%S')),
+        ]
+    }
+    assert pm.average_time_differences(eventlog) == {'t1': {'t2': dti.timedelta(seconds=1)}}
