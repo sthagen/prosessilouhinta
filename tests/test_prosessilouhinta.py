28dcb16683183be48e97471569c297a6d49cf594
Stefan Hagen <stefan@hagen.link>
2021-11-27 20:51:52 +0100

    Added another implementation test

--- tests/test_prosessilouhinta.py
+++ tests/test_prosessilouhinta.py
@@ -7,6 +7,11 @@ import prosessilouhinta.prosessilouhinta as pm
 FIXTURES_PATH = pathlib.Path('tests', 'fixtures')
 
 
-def test_parse_eventlog_csv():
+def test_parse_empty_eventlog_csv():
     empty = FIXTURES_PATH / 'basic' / 'empty.csv'
+    assert pm.parse_eventlog_csv(empty) == {}
+
+
+def test_parse_header_only_eventlog_csv():
+    empty = FIXTURES_PATH / 'basic' / 'header-only.csv'
     assert pm.parse_eventlog_csv(empty) == {}
