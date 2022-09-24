d8d9d56c086d2a4f72c86de6a41dcf688a3b08dd
Stefan Hagen <stefan@hagen.link>
2022-09-24 23:31:34 +0200

    Bumped impl version for release

--- prosessilouhinta/__init__.py
+++ prosessilouhinta/__init__.py
@@ -2,8 +2,8 @@
 from typing import List
 
 # [[[fill git_describe()]]]
-__version__ = '2022.9.21+parent.339872ae'
-# [[[end]]] (checksum: 8494bc4af92413e2f783961b0b7192d2)
+__version__ = '2022.9.24+parent.c701f999'
+# [[[end]]] (checksum: b7ecf16b869e85e50c9e3e73bcf36510)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
