3436a4cee542480f729c9d6c915928eedc10e4a5
Stefan Hagen <stefan@hagen.link>
2022-09-24 23:27:29 +0200

    Inserted version hook

--- prosessilouhinta/__init__.py
+++ prosessilouhinta/__init__.py
@@ -1,2 +1,10 @@
 """Process mining (Finnish prosessilouhinta) from eventlogs. Version and init interface."""
-__version__ = '2021.11.27'
+from typing import List
+
+# [[[fill git_describe()]]]
+__version__ = '2022.9.21+parent.339872ae'
+# [[[end]]] (checksum: 8494bc4af92413e2f783961b0b7192d2)
+__version_info__ = tuple(
+    e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
+)
+__all__: List[str] = []
